09e35024166410a88eaf25b1c7e3ffe6185a54ac
rlaxoduq001 <rlaxoduq001@gmail.com>
2022-04-26 23:22:12 +0900

    옛날 클래스문법, props

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,13 +2,15 @@
 
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
 
   let [ title, titleState ] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
   let [ like , likeState ] = useState(0);
   let [ modal, setModal ] = useState(false);
+  let [ titleCnt, setTitleCnt ] = useState(0);
+  let [ inputVal , setInputVal ] = useState('');
 
   return (
     <div className="App">
@@ -45,36 +47,94 @@ function App() {
       </div> */}
 
       {
-        title.map( test => {
+        title.map( ( compTitle,i) => {
+          
           return (
-            <div className="list" key={test}>
-              <span onClick={ () => { likeState( like+1 ) }}> 👍 </span> {like}
-              <h2 onClick={ () => { setModal(!modal) }} >{ test }</h2>
+            <div className="list" key={i}>
+              <h2 onClick={ () => { 
+                setModal(!modal);
+                setTitleCnt(i);}} >
+                  { compTitle }
+              <span onClick={ (e) => { 
+                likeState( like+1 )
+                e.stopPropagation();
+              }}> 👍 </span> {like}
+              </h2>
               <p>2월 17일 발행</p>
+
+              <button onClick={() => {
+                let copy = [...title];
+                copy.splice(i, 1);
+                titleState(copy);
+              }}>삭제</button>
             </div>
           )
         })
       }
 
-
+      <input onChange={ (e) => {
+        setInputVal(e.target.value);
+        console.log(inputVal);
+      }}></input>
+      
+      <button onClick={() => {
+        let copy = [...title];
+        copy.unshift('asd');
+        titleState(copy);
+      }}>저장저장</button>
 
 
       {
-        modal == true ? <Modal/> : null
+        modal == true ? 
+        <Modal color={'skyblue'} 
+               title={ title } 
+               title2={ titleCnt }
+               /> : null
       }
 
+    <Profile></Profile>
+
     </div>
+
+    
   );
 }
 
-const Modal = () => {
+const Modal = ( props ) => {
   return (
-    <div className='modal'>
-      <h4>제목</h4>
-      <p>날짜</p>
+    <div className='modal' style={{ background :  props.color}}>
+      <h4>{props.title[props.title2]}</h4>
+      <p>날짜</p> 
       <p>상세내용</p>
     </div>
   )
+}
+
+
+// 리액트 Class 문법
+class Profile extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = { name : 'Kim', age : 30 }
+  }
+
+  changeName() {
+    this.setState( {name : 'park'} );
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>프로필</h1>
+        <p>저는 {this.state.name} 입니다. 나이는 {this.state.age}</p>
+        <button onClick={ () => {
+          this.changeName();
+        }}>버튼</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
